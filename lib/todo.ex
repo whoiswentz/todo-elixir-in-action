734a2f3be77f7a8ccd364a9880e720dd46cd68d5
Vinicios Wentz <vinicios.wentz@icloud.com>
2024-03-09 14:00:45 -0300

    simple todo crud

--- lib/todo.ex
+++ lib/todo.ex
@@ -1,18 +1,34 @@
 defmodule Todo do
-  @moduledoc """
-  Documentation for `Todo`.
-  """
+  defstruct next_id: 1, entries: %{}
 
-  @doc """
-  Hello world.
+  def new(entries \\ []) do
+    Enum.reduce(entries, %Todo{}, &add_entry(&2, &1))
+  end
 
-  ## Examples
+  def add_entry(todo_list, entry) do
+    new_entry = Map.put(entry, :id, todo_list.next_id + 1)
+    new_entries = Map.put(todo_list.entries, todo_list.next_id, new_entry)
+    %Todo{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
+  end
 
-      iex> Todo.hello()
-      :world
+  def update_entry(todo_list, entry_id, update_function) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error -> todo_list
+      {:ok, old_entry} ->
+        new_entry = update_function.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %Todo{todo_list | entries: new_entries}
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %Todo{todo_list | entries: new_entries}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Map.values()
+    |> Enum.filter(&(&1.date == date))
   end
 end
